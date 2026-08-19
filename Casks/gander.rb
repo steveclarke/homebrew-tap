5e2309aa82259a444accdbeac21b93b672ea9f34
Steve Clarke <steve@sevenview.ca>
2026-08-19 14:10:27 -0230

    Update Gander to 0.1.11

--- Casks/gander.rb
+++ Casks/gander.rb
@@ -1,6 +1,6 @@
 cask "gander" do
-  version "0.1.10"
-  sha256 "1e39cf081baa6aea1c60093675692cf3537d809486f073f31481635fe95e0266"
+  version "0.1.11"
+  sha256 "210e722166bdc42b744c38469eb7472d613c48accaf645ccdcd4c364ed83aabd"
 
   url "https://github.com/steveclarke/gander/releases/download/v#{version}/Gander-#{version}-arm64.dmg"
   name "Gander"
